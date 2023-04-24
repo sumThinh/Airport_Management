3e507c4fb4dab075dfec887a6081c7f9c07a2827
Nguyễn Trường Phước Thịnh <pthinh6@gmail.com>
2023-04-24 09:40:40 +0700

    Update Bill

---
+++ Airport_Manager/BUS/BUS_Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+
+namespace BUS
+{
+    internal class BUS_Ticket
+    {
+        private static DAL_Ticket ticketbill = new DAL_Ticket();
+
+        public List<Bill_Detail> GetListBills() => ticketbill.LoadBills();
+        public static bool AddBillService(Bill_Detail tick)
+        {
+            return ticketbill.AddTicket(tick);
+        }
+
+        public static bool DeleteBillService(Bill_Detail tick)
+        {
+            return ticketbill.RemoveTicket(tick.BillID);
+        }
+    }
+}
---
+++ Airport_Manager/DAL/DAL_Ticket.cs
@@ -0,0 +1,39 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_Ticket
+    {
+        public List<Bill_Detail> LoadBills()
+        {
+            var list = new List<Bill_Detail>();
+            using (var db = new AirportManager())
+                list = db.Bill_Detail.ToList();
+            return list;
+        }
+
+        public bool AddTicket(Bill_Detail tick)
+        {
+            using (var db = new AirportManager())
+            {
+                db.Bill_Detail.Add(tick);
+                return db.SaveChanges() > 0;
+            }
+        }
+
+        public bool RemoveTicket(int id)
+        {
+            using (var db = new AirportManager())
+            {
+                var targetedBill = (from d in db.Bill_Detail where d.BillID == id select d).SingleOrDefault();
+                db.Bill_Detail.Remove(targetedBill);
+                return db.SaveChanges() > 0;
+            }
+        }
+    }
+}
